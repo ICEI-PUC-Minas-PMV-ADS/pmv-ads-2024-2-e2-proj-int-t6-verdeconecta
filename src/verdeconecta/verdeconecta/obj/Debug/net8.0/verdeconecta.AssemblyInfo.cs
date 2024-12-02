438ccb81cc4b7f73a08cfc5e8626c9a50ad068b1
FelipeKutschera <165940217+FelipeKutschera@users.noreply.github.com>
2024-12-02 19:32:37 -0300

    hopedagem

--- src/verdeconecta/verdeconecta/obj/Debug/net8.0/verdeconecta.AssemblyInfo.cs
+++ src/verdeconecta/verdeconecta/obj/Debug/net8.0/verdeconecta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d831e415ca1dd6efba0e9f4e3721311d9ef426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762f636ff9eb5792620f4cf4153c879d9ac7e49c")]
 [assembly: System.Reflection.AssemblyProductAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
