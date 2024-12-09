2904f87ca62c24021d2f5bf5432e9edaca503815
Nicolas Gabriel <contato.nicolasneres@gmail.com>
2024-12-09 19:05:58 -0300

    Voltando commit

--- src/verdeconecta/verdeconecta/obj/Debug/net8.0/verdeconecta.AssemblyInfo.cs
+++ src/verdeconecta/verdeconecta/obj/Debug/net8.0/verdeconecta.AssemblyInfo.cs
@@ -15,11 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79a2c83822600307c0a0f38e03867e4522a8bed")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c6bf7c18e54d5a269b0d75f5d90878255573b0")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a618564dc72336a3a93e64ea0a71eb538c1f44d")]
 [assembly: System.Reflection.AssemblyProductAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("verdeconecta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
