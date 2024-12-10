7a17ea67481b7b8420d35f10dbadde04b9fc2817
junioflausino <164429443+junioflausino@users.noreply.github.com>
2024-12-10 14:21:40 -0300

    Adicionar comentarios ao contexto

--- src/verdeconecta/verdeconecta/Models/AppDbContext.cs
+++ src/verdeconecta/verdeconecta/Models/AppDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace verdeconecta.Models
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Alimento> Alimentos { get; set; }
         public DbSet<Refeicao> Refeicoes { get; set; }
@@ -14,6 +14,9 @@ namespace verdeconecta.Models
         public DbSet<DicasNutricionais> DicasNutricionais { get; set; }
 
         public DbSet<RelacionamentoNutricionistaCliente> RelacionamentoNutricionistaCliente { get; set; }
+
+        // Adicionando DbSet para Comentario
+        public DbSet<Comentario> Comentarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -27,12 +30,25 @@ namespace verdeconecta.Models
                 .HasForeignKey(r => r.IdNutricionista)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            modelBuilder.Entity<Comentario>()
+                .HasOne(c => c.Refeicao)  // Relacionamento com Refeição
+                .WithMany(r => r.Comentarios)  // Refeição pode ter muitos Comentarios
+                .HasForeignKey(c => c.RefeicaoId)  // A chave estrangeira é RefeicaoId
+                .OnDelete(DeleteBehavior.Cascade);  // Comportamento de exclusão
+
+            modelBuilder.Entity<Comentario>()
+                .HasOne(c => c.Nutricionista)  // Relacionamento com Nutricionista
+                .WithMany()  // A relação reversa não precisa ser explícita
+                .HasForeignKey(c => c.NutricionistaId)  // Chave estrangeira NutricionistaId
+                .OnDelete(DeleteBehavior.Restrict);  // Evitar exclusão em cascata do nutricionista
+
+
             modelBuilder.Entity<RelacionamentoNutricionistaCliente>()
                 .HasOne(r => r.Cliente)
                 .WithMany()
                 .HasForeignKey(r => r.IdCliente)
                 .OnDelete(DeleteBehavior.Restrict);
         }
-       public DbSet<Questionario> Questionarios { get; set; }
+        public DbSet<Questionario> Questionarios { get; set; }
     }
 }
